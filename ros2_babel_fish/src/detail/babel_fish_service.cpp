e667276fd00b0f7cd98e1fb79188546c9f2c54b5
Stefan Fabian <github@stefanfabian.com>
2022-09-13 16:18:06 +0200

    Simplified service handle cleanup in accordance to default implementation.

--- ros2_babel_fish/src/detail/babel_fish_service.cpp
+++ ros2_babel_fish/src/detail/babel_fish_service.cpp
@@ -17,29 +17,17 @@ BabelFishService::BabelFishService( std::shared_ptr<rcl_node_t> node_base,
                                     rcl_service_options_t options )
   : ServiceBase( std::move( node_base )), type_support_( std::move( type_support )), callback_( callback )
 {
-  std::weak_ptr<rcl_node_t> weak_node_handle( node_handle_ );
   // rcl does the static memory allocation here
   service_handle_ = std::shared_ptr<rcl_service_t>(
-    new rcl_service_t, [ weak_node_handle ]( rcl_service_t *service )
+    new rcl_service_t, [ handle = node_handle_ ]( rcl_service_t *service )
     {
-      auto handle = weak_node_handle.lock();
-      if ( handle )
-      {
-        if ( rcl_service_fini( service, handle.get()) != RCL_RET_OK )
-        {
-          RCLCPP_ERROR(
-            rclcpp::get_node_logger( handle.get()).get_child( "rclcpp" ),
-            "Error in destruction of rcl service handle: %s",
-            rcl_get_error_string().str );
-          rcl_reset_error();
-        }
-      }
-      else
+      if ( rcl_service_fini( service, handle.get()) != RCL_RET_OK )
       {
         RCLCPP_ERROR(
-          rclcpp::get_logger( "rclcpp" ),
-          "Error in destruction of rcl service handle: "
-          "the Node Handle was destructed too early. You will leak memory" );
+          rclcpp::get_node_logger( handle.get()).get_child( "rclcpp" ),
+          "Error in destruction of rcl service handle: %s",
+          rcl_get_error_string().str );
+        rcl_reset_error();
       }
       delete service;
     } );
@@ -69,8 +57,8 @@ BabelFishService::BabelFishService( std::shared_ptr<rcl_node_t> node_base,
   }
   TRACEPOINT(
     rclcpp_service_callback_added,
-    (const void *) get_service_handle().get(),
-    (const void *) &callback_ );
+    static_cast<const void *>(get_service_handle().get()),
+    static_cast<const void *>(&callback_) );
 #ifndef TRACETOOLS_DISABLED
   callback_.register_callback_for_tracing();
 #endif
