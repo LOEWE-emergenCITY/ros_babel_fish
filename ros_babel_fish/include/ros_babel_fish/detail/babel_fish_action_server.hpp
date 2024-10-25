d4a21b79fd7db80e765dc076fb8ce7721abb9b43
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2024-10-25 15:26:39 +0200

    Add ActionServer (#9)
    * More verbose error message when trying to assign a value to a CompoundMessage.
    
    * Moved BabelFishAction definition to separate header in preparation for BabelFishActionServer.
    
    * Added BabelFishActionServer with tests.
    
    * Suppress false positive in cppcheck.
    
    * Enable inline suppression in cppcheck.
    
    (cherry picked from commit 0d65c2dfd7b1c402970b0512c530ceaee71fd8b1)

--- ros_babel_fish/include/ros_babel_fish/detail/babel_fish_action_server.hpp
+++ ros_babel_fish/include/ros_babel_fish/detail/babel_fish_action_server.hpp
@@ -1,33 +1,138 @@
 // Copyright (c) 2021 Stefan Fabian. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#ifndef ROS_BABEL_FISH_BABEL_FISH_ACTION_HPP
-#define ROS_BABEL_FISH_BABEL_FISH_ACTION_HPP
+#ifndef ROS_BABEL_FISH_BABEL_FISH_ACTION_SERVER_HPP
+#define ROS_BABEL_FISH_BABEL_FISH_ACTION_SERVER_HPP
 
-#include <rclcpp/ac.hpp>
+#include "ros_babel_fish/detail/babel_fish_action.hpp"
+#include <rclcpp_action/server.hpp>
 #include <ros_babel_fish/idl/type_support.hpp>
 #include <ros_babel_fish/messages/compound_message.hpp>
 
-namespace ros_babel_fish
+namespace rclcpp_action
 {
 
-class BabelFishService
+template<>
+class ServerGoalHandle<ros_babel_fish::impl::BabelFishAction>
+    : public ServerGoalHandleBase,
+      public std::enable_shared_from_this<ServerGoalHandle<ros_babel_fish::impl::BabelFishAction>>
 {
 public:
-  RCLCPP_SMART_PTR_DEFINITIONS( BabelFishService )
+  using ActionT = ros_babel_fish::impl::BabelFishAction;
 
-  BabelFishService(
-      rclcpp::Node *node, const std::string &name, ServiceTypeSupport::ConstSharedPtr type_support,
-      std::function<void( const rmw_request_id_t &, const CompoundMessage &, CompoundMessage & )> callback,
-      rcl_service_options_t options );
+  /// @see ServerGoalHandle<ActionT>::publish_feedback
+  void publish_feedback( const ActionT::Feedback &feedback_msg ) const;
 
-  rclcpp::ServiceBase::ConstSharedPtr getService() const;
+  /// @see ServerGoalHandle<ActionT>::abort
+  void abort( const ActionT::Result &result_msg );
 
-  rclcpp::ServiceBase::SharedPtr getService();
+  /// @see ServerGoalHandle<ActionT>::succeed
+  void succeed( const ActionT::Result &result_msg );
+
+  // @see ServerGoalHandle<ActionT>::canceled
+  void canceled( const ActionT::Result &result_msg );
+
+  /// @see ServerGoalHandle<ActionT>::execute
+  void execute();
+
+  /// Get the user provided message describing the goal.
+  const std::shared_ptr<const ActionT::Goal> &get_goal() const { return goal_; }
+
+  /// Get the unique identifier of the goal
+  const GoalUUID &get_goal_id() const { return uuid_; }
+
+  ~ServerGoalHandle() override;
+
+  ActionT::Result create_result_message() const;
+
+  ActionT::Feedback create_feedback_message() const;
 
 private:
-  rclcpp::ServiceBase::SharedPtr service_;
+  ServerGoalHandle( ros_babel_fish::ActionTypeSupport::ConstSharedPtr type_support,
+                    std::shared_ptr<rcl_action_goal_handle_t> rcl_handle, GoalUUID uuid,
+                    std::shared_ptr<const typename ActionT::Goal> goal,
+                    std::function<void( const GoalUUID &, std::shared_ptr<void> )> on_terminal_state,
+                    std::function<void( const GoalUUID & )> on_executing,
+                    std::function<void( std::shared_ptr<void> )> publish_feedback );
+
+  ros_babel_fish::ActionTypeSupport::ConstSharedPtr type_support_;
+  const std::shared_ptr<const ActionT::Goal> goal_;
+  const GoalUUID uuid_;
+
+  friend class Server<ActionT>;
+
+  std::function<void( const GoalUUID &, std::shared_ptr<void> )> on_terminal_state_;
+  std::function<void( const GoalUUID & )> on_executing_;
+  std::function<void( std::shared_ptr<void> )> publish_feedback_;
 };
+
+template<>
+class Server<ros_babel_fish::impl::BabelFishAction>
+    : public ServerBase,
+      public std::enable_shared_from_this<Server<ros_babel_fish::impl::BabelFishAction>>
+{
+public:
+  RCLCPP_SMART_PTR_DEFINITIONS_NOT_COPYABLE( Server )
+  using ActionT = ros_babel_fish::impl::BabelFishAction;
+  using GoalHandle = ServerGoalHandle<ActionT>;
+
+  using GoalCallback =
+      std::function<GoalResponse( const GoalUUID &, std::shared_ptr<const ActionT::Goal> )>;
+
+  using CancelCallback =
+      std::function<CancelResponse( const std::shared_ptr<ServerGoalHandle<ActionT>> )>;
+
+  using AcceptedCallback = std::function<void( std::shared_ptr<ServerGoalHandle<ActionT>> )>;
+
+  //! Do not call directly, this is private API and might change. Use BabelFish::create_action_server.
+  Server( rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_base,
+          rclcpp::node_interfaces::NodeClockInterface::SharedPtr node_clock,
+          rclcpp::node_interfaces::NodeLoggingInterface::SharedPtr node_logging,
+          const std::string &name, ros_babel_fish::ActionTypeSupport::ConstSharedPtr type_support,
+          const rcl_action_server_options_t &options, GoalCallback handle_goal,
+          CancelCallback handle_cancel, AcceptedCallback handle_accepted );
+
+  ~Server() override;
+
+private:
+  std::pair<GoalResponse, std::shared_ptr<void>>
+  call_handle_goal_callback( GoalUUID &uuid, std::shared_ptr<void> message ) override;
+
+  CancelResponse call_handle_cancel_callback( const GoalUUID &uuid ) override;
+
+  void call_goal_accepted_callback( std::shared_ptr<rcl_action_goal_handle_t> rcl_goal_handle,
+                                    GoalUUID uuid,
+                                    std::shared_ptr<void> goal_request_message ) override;
+
+  GoalUUID get_goal_id_from_goal_request( void *message ) override;
+
+  std::shared_ptr<void> create_goal_request() override;
+
+  GoalUUID get_goal_id_from_result_request( void *message ) override;
+
+  std::shared_ptr<void> create_result_request() override;
+
+  std::shared_ptr<void>
+  create_result_response( decltype( action_msgs::msg::GoalStatus::status ) status ) override;
+
+  ros_babel_fish::ActionTypeSupport::ConstSharedPtr type_support_;
+
+  GoalCallback handle_goal_;
+  CancelCallback handle_cancel_;
+  AcceptedCallback handle_accepted_;
+
+  using GoalHandleWeakPtr = std::weak_ptr<ServerGoalHandle<ActionT>>;
+  /// A map of goal id to goal handle weak pointers.
+  /// This is used to provide a goal handle to handle_cancel.
+  std::unordered_map<GoalUUID, GoalHandleWeakPtr> goal_handles_;
+  std::mutex goal_handles_mutex_;
+};
+} // namespace rclcpp_action
+
+namespace ros_babel_fish
+{
+using BabelFishActionServer = rclcpp_action::Server<impl::BabelFishAction>;
+using BabelFishActionServerGoalHandle = rclcpp_action::ServerGoalHandle<impl::BabelFishAction>;
 } // namespace ros_babel_fish
 
 #endif // ROS_BABEL_FISH_BABEL_FISH_ACTION_HPP
--- ros_babel_fish/test/action_server.cpp
+++ ros_babel_fish/test/action_server.cpp
@@ -1,17 +1,15 @@
-//
-// Created by Stefan Fabian on 03.03.20.
-//
+// Copyright (c) 2024 Stefan Fabian. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include "message_comparison.hpp"
 #include <ros_babel_fish/babel_fish.hpp>
 
 #include <action_msgs/msg/goal_status_array.h>
-#include <action_tutorials_interfaces/action/fibonacci.hpp>
+#include <example_interfaces/action/fibonacci.hpp>
 #include <ros_babel_fish_test_msgs/action/simple_test.hpp>
 
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
-#include <rosidl_typesupport_c/type_support_map.h>
 
 using namespace ros_babel_fish;
 using namespace ros_babel_fish_test_msgs;
@@ -37,78 +35,55 @@ rclcpp::Node::SharedPtr node;
   return result;
 }
 
-TEST( ActionClientTest, actionLookup )
-{
-  BabelFish fish;
-  ActionTypeSupport::ConstSharedPtr type_support =
-      fish.get_action_type_support( "ros_babel_fish_test_msgs/action/SimpleTest" );
-  ASSERT_NE( type_support, nullptr );
-
-  const rosidl_action_type_support_t *ts = rosidl_typesupport_cpp::get_action_type_support_handle<
-      ros_babel_fish_test_msgs::action::SimpleTest>();
-  EXPECT_NE( ts, nullptr );
-  EXPECT_TRUE( Equal( type_support->type_support_handle, *ts ) );
-
-  type_support = fish.get_action_type_support( "action_tutorials_interfaces/action/Fibonacci" );
-  ASSERT_NE( type_support, nullptr );
-
-  const auto *ts_map =
-      static_cast<const type_support_map_t *>( ts->status_message_type_support->data );
-  const auto *map = static_cast<const type_support_map_t *>(
-      type_support->status_message_type_support->type_support_handle.data );
-  EXPECT_EQ( ts_map, map );
-}
-
-TEST( ActionClientTest, actionClient )
+TEST( ActionClientTest, actionServer )
 {
   using namespace std::chrono_literals;
   using Action = ros_babel_fish_test_msgs::action::SimpleTest;
-  rclcpp_action::Server<Action>::SharedPtr server = rclcpp_action::create_server<Action>(
-      node, "ros_babel_fish_client_test_server",
-      []( const rclcpp_action::GoalUUID &, const std::shared_ptr<const Action::Goal> &goal ) {
-        if ( goal->target == 13 ) // 13 is rejected
+  BabelFish fish;
+  auto server = fish.create_action_server(
+      *node, "ros_babel_fish_server_test_server", "ros_babel_fish_test_msgs/action/SimpleTest",
+      []( const rclcpp_action::GoalUUID &, const CompoundMessage::ConstSharedPtr &msg ) {
+        if ( ( *msg )["target"] == 13 ) // 13 is rejected
           return rclcpp_action::GoalResponse::REJECT;
         return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
       },
-      []( const std::shared_ptr<rclcpp_action::ServerGoalHandle<Action>> &handle ) {
-        if ( handle->get_goal()->target == 7 ) // 7 can't be canceled
+      []( const std::shared_ptr<BabelFishActionServerGoalHandle> &handle ) {
+        if ( ( *handle->get_goal() )["target"].value<int32_t>() == 7 )
           return rclcpp_action::CancelResponse::REJECT;
         return rclcpp_action::CancelResponse::ACCEPT;
       },
-      []( const std::shared_ptr<rclcpp_action::ServerGoalHandle<Action>> &handle ) {
+      []( const std::shared_ptr<BabelFishActionServerGoalHandle> &handle ) {
         std::thread t( [handle]() {
-          auto result = std::make_shared<Action::Result>();
-          for ( int i = 0; i < handle->get_goal()->target; ++i ) {
-            if ( i >= 10 && i >= handle->get_goal()->target / 2 ) {
-              result->final_value = i;
+          auto result = handle->create_result_message();
+          int goal = ( *handle->get_goal() )["target"].value<int32_t>();
+          for ( int i = 0; i < goal; ++i ) {
+            if ( i >= 10 && i >= goal / 2 ) {
+              result["final_value"] = i;
               handle->abort( result );
               return;
             }
             std::this_thread::sleep_for( 50ms );
-            auto feedback = std::make_shared<Action::Feedback>();
-            feedback->current_value = i;
+            auto feedback = handle->create_feedback_message();
+            feedback["current_value"] = i;
             handle->publish_feedback( feedback );
             if ( handle->is_canceling() ) {
-              result->final_value = i;
+              result["final_value"] = i;
               handle->canceled( result );
               return;
             }
           }
-          result->final_value = handle->get_goal()->target;
+          result["final_value"] = goal;
           handle->succeed( result );
         } );
         t.detach();
       } );
-
-  BabelFish fish;
-  auto client = fish.create_action_client( *node, "ros_babel_fish_client_test_server",
-                                           "ros_babel_fish_test_msgs/action/SimpleTest" );
+  auto client = rclcpp_action::create_client<Action>( node, "ros_babel_fish_server_test_server" );
   ASSERT_TRUE( client->wait_for_action_server( 5s ) );
   ASSERT_TRUE( client->action_server_is_ready() );
 
   // This goal should succeed
-  auto goal = client->create_goal();
-  goal["target"] = 2;
+  Action::Goal goal;
+  goal.target = 2;
   auto gh_future = client->async_send_goal( goal );
   ASSERT_EQ( gh_future.wait_for( 30s ), std::future_status::ready );
   auto goal_handle = gh_future.get();
@@ -118,19 +93,17 @@ TEST( ActionClientTest, actionClient )
   ASSERT_EQ( result.wait_for( 3s ), std::future_status::ready );
   ASSERT_EQ( goal_handle->get_status(), action_msgs::msg::GoalStatus::STATUS_SUCCEEDED );
   auto result_msg = result.get();
-  EXPECT_EQ( ( *result_msg.result )["final_value"].value<int32_t>(), 2 );
+  EXPECT_EQ( result_msg.result->final_value, 2 );
 
   // This goal should abort after 10
-  goal = client->create_goal();
-  goal["target"] = 20;
+  goal.target = 20;
   std::vector<int> feedback_values;
-  gh_future = client->async_send_goal(
-      goal, { {},
-              [&feedback_values]( const BabelFishActionClient::GoalHandle::SharedPtr &,
-                                  const CompoundMessage::ConstSharedPtr &feedback ) {
-                feedback_values.push_back( ( *feedback )["current_value"].value<int32_t>() );
-              },
-              {} } );
+  rclcpp_action::Client<Action>::SendGoalOptions options;
+  options.feedback_callback =
+      [&feedback_values]( auto, const std::shared_ptr<const Action::Feedback> &feedback ) {
+        feedback_values.push_back( feedback->current_value );
+      };
+  gh_future = client->async_send_goal( goal, options );
   ASSERT_EQ( gh_future.wait_for( 3s ), std::future_status::ready );
   goal_handle = gh_future.get();
   ASSERT_NE( goal_handle, nullptr );
@@ -139,7 +112,7 @@ TEST( ActionClientTest, actionClient )
   ASSERT_EQ( result.wait_for( 10s ), std::future_status::ready );
   ASSERT_EQ( goal_handle->get_status(), action_msgs::msg::GoalStatus::STATUS_ABORTED );
   result_msg = result.get();
-  EXPECT_EQ( ( *result_msg.result )["final_value"].value<int32_t>(), 10 );
+  EXPECT_EQ( result_msg.result->final_value, 10 );
 
   ASSERT_EQ( feedback_values.size(), 10U );
   for ( int i = 0; i < 10; ++i ) {
@@ -148,8 +121,7 @@ TEST( ActionClientTest, actionClient )
   }
 
   // This goal should be preempted
-  goal = client->create_goal();
-  goal["target"] = 200;
+  goal.target = 200;
   gh_future = client->async_send_goal( goal );
   ASSERT_EQ( gh_future.wait_for( 3s ), std::future_status::ready );
   goal_handle = gh_future.get();
@@ -159,25 +131,22 @@ TEST( ActionClientTest, actionClient )
   ASSERT_EQ( cancel_response_future.wait_for( 1s ), std::future_status::ready )
       << "ActionServer did not cancel in 1 second!";
   auto cancel_response = cancel_response_future.get();
-  ASSERT_EQ( cancel_response["return_code"].value<int8_t>(),
-             action_msgs::srv::CancelGoal::Response::ERROR_NONE );
+  ASSERT_EQ( cancel_response->return_code, action_msgs::srv::CancelGoal::Response::ERROR_NONE );
   result = client->async_get_result( goal_handle );
   ASSERT_EQ( result.wait_for( 10s ), std::future_status::ready );
   EXPECT_EQ( goal_handle->get_status(), action_msgs::msg::GoalStatus::STATUS_CANCELED );
   result_msg = result.get();
-  EXPECT_LT( ( *result_msg.result )["final_value"].value<int32_t>(), 200 );
+  EXPECT_LT( result_msg.result->final_value, 200 );
 
   // 13 should be rejected
-  goal = client->create_goal();
-  goal["target"] = 13;
+  goal.target = 13;
   gh_future = client->async_send_goal( goal );
   ASSERT_EQ( gh_future.wait_for( 3s ), std::future_status::ready );
   goal_handle = gh_future.get();
   ASSERT_EQ( goal_handle, nullptr );
 
   // 7 can't be canceled
-  goal = client->create_goal();
-  goal["target"] = 7;
+  goal.target = 7;
   gh_future = client->async_send_goal( goal );
   ASSERT_EQ( gh_future.wait_for( 3s ), std::future_status::ready );
   goal_handle = gh_future.get();
@@ -188,8 +157,7 @@ TEST( ActionClientTest, actionClient )
   ASSERT_EQ( cancel_response_future.wait_for( 1s ), std::future_status::ready )
       << "ActionServer did not cancel in 1 second!";
   cancel_response = cancel_response_future.get();
-  ASSERT_EQ( cancel_response["return_code"].value<int8_t>(),
-             action_msgs::srv::CancelGoal::Response::ERROR_REJECTED );
+  ASSERT_EQ( cancel_response->return_code, action_msgs::srv::CancelGoal::Response::ERROR_REJECTED );
   result = client->async_get_result( goal_handle );
   ASSERT_EQ( result.wait_for( 10s ), std::future_status::ready );
 }
@@ -198,7 +166,7 @@ int main( int argc, char **argv )
 {
   testing::InitGoogleTest( &argc, argv );
   rclcpp::init( argc, argv );
-  node = std::make_shared<rclcpp::Node>( "action_client_test" );
+  node = std::make_shared<rclcpp::Node>( "action_server_test" );
   std::thread spinner( []() { rclcpp::spin( node ); } );
   int result = RUN_ALL_TESTS();
   rclcpp::shutdown();
