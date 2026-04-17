fb6bca84904f1510f88f9631feb9f60de248b1cc
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2026-04-17 11:53:46 +0000

    Updated docs.

--- ros_babel_fish_tools/test/nlohmann_json_serialization.cpp
+++ ros_babel_fish_tools/test/nlohmann_json_serialization.cpp
@@ -548,8 +548,5 @@ TEST_F( JsonSerializationTest, nullValuesIgnored )
 int main( int argc, char **argv )
 {
   testing::InitGoogleTest( &argc, argv );
-  rclcpp::init( argc, argv );
-  int result = RUN_ALL_TESTS();
-  rclcpp::shutdown();
-  return result;
+  return RUN_ALL_TESTS();
 }
